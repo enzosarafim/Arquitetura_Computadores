c2c9164cc680e66b463c795abc0aaf2a8419fa6a
GabrieIing <gabrielzarour@outlook.com>
2026-08-27 20:02:26 -0300

    Modify loop logic and add number variable

--- Laboratorio_2/atividade_6.s
+++ Laboratorio_2/atividade_6.s
@@ -1,17 +1,19 @@
 	lw x11, HIGH
 	lw x12, LOW
+	lw x13, number
 loop:
 	lb x10, 1026(x0)
-	andi x10, x10, 1
-	beq x10, x0, off
+	andi x10, x10, 1 
 on:
 	sb x11, 1029(x0)
-	slli x11, x11, 1
+	lb x10, 1026(x0)
+	beq x10, x13, off
 	jal x0, loop
 off:
+	slli x11, x11, 1
 	sb x12, 1029(x0)
 	jal x0, loop
 
 HIGH: .byte 1
 LOW:  .byte 0
-
+number: .byte 1
